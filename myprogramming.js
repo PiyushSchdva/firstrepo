d61ac7d01dd45e127fe08e59a2e6743d3e6ec8e1
Piyush Sachdeva <PiyushSchdva>
2025-07-31 13:47:11 +0530

    First commit

---
+++ myprogramming.js
@@ -0,0 +1,89 @@
+// console.log("This is my testing of printing text.");
+// fullname = "Your name";
+// console.log(fullname);
+
+// const student = {
+//     age:10, cgpa:8.5
+// }
+
+// console.log(student.age, student["age"]);
+
+
+
+// let number = "Eight";
+// console.log(number.split(""));
+
+// console.log(number.split("").reverse());
+
+
+
+// let student_names = ["Mohit", "Aman", "Rahul" ];
+
+// console.log (student_names);
+
+// for(let idx=0; idx<student_names.length;idx++){
+//     console.log(student_names[idx]);
+// }
+
+// function forLargestNumber (listOfNumbers){
+//     let largest = listOfNumbers[0];
+//     for(idx=0; idx<listOfNumbers.length;idx++){
+//     if (listOfNumbers[idx]>largest){
+        largest=listOfNumbers[idx];
+        
+        
+//     }
+// }
+// return (largest);
+// }
+
+// let firstArray = [20,40,60,80,100,30];
+// console.log(forLargestNumber(firstArray));
+
+
+// if( username === "bob"){
+//     console.log("Hi Bob")
+// }
+// else{
+//     "No, try again"
+// }
+
+
+
+
+// var age = prompt("What is your age?");
+
+// if (Number(age) < 18) {
+// 	alert("Sorry, you are too yound to drive this car. Powering off");
+// } else if (Number(age) > 18) {
+// 	alert("Powering On. Enjoy the ride!");
+// } else if (Number(age) === 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// }
+
+
+// function checkDriverAge(a){
+//     if (Number(a) < 18) {
+// 	alert("Sorry, you are too young to drive this car. Powering off");
+// } else if (Number(a) > 18) {
+// 	alert("Powering On. Enjoy the ride!");
+// } else if (Number(a) === 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// }
+// }
+
+// checkDriverAge(20);
+
+
+// console.log("This file is working now.");
+
+
+// for (i= 0; i<10;i++){
+//     console.log(i);
+    
+// }
+
+// let vari  = "Hello";
+// console.log(vari.toUpperCase());
+
+
---
+++ practicequestions.js
@@ -0,0 +1,138 @@
+// Que-1:
+// function toreverseString(name) {
+//   return name.split('').reverse();
+
+// }
+// console.log(toreverseString("Piyush"));
+
+
+// Q-3 Write a function that returns the largest number from a given array.
+
+// let myArray=[1,2,3,4,80,60,70];
+// let largestNumber = myArray[0]
+// for(idx=0; idx<myArray.length;idx++){
+//     if (myArray[idx]>largestNumber){
+//         largestNumber=myArray[idx];
+        
+        
+//     }
+// }
+// console.log(largestNumber);
+
+// function forLargestNumber (listOfNumbers){
+//     let largest = listOfNumbers[0];
+//     for(idx=0; idx<listOfNumbers.length;idx++){
+//     if (listOfNumbers[idx]>largest){
+//         largest=listOfNumbers[idx];
+        
+        
+//     }
+// }
+// return (largest);
+// }
+
+// let firstArray = [20,40,60,80,100,30];
+// console.log(forLargestNumber(firstArray));
+
+
+
+// Q-4 Write a function that takes an array of numbers and returns the sum of all numbers.
+
+// const numbers = [10,20,30,40];
+// let sum = 0;
+// for (let index = 0; index < numbers.length; index++) {
+//             sum = sum+numbers[index];
+            
+// }
+// console.log(sum);
+
+// function add (values) {
+//     let sum = 0;
+// for (let index = 0; index < numbers.length; index++) {
+//             sum = sum+numbers[index];
+            
+// }
+// return sum;
+// }
+
+// let numericArray = [30,60,10];
+// console.log(add(numericArray));
+
+
+// Q-2 Write a function to check if a given string is a palindrome (reads the same backward as forward).
+
+
+
+
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+
+    
+//     if (reversedArray{}==element){
+//      console.log("This is a palindrome string.")   
+//     }
+
+//         else{
+//             console.log("This is not a palindrome string.")   
+//         }
+
+    
+// }
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     arrayCharacters = arr[i];        
+//         console.log(arrayCharacters);
+//     }
+
+// for (let j = 0; j < reversedArray.length; j++) {
+//     reversedArrayCharacters = reversedArray[j];        
+//         console.log(reversedArrayCharacters);
+//     }
+    
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < reversedArray.length; j++) {
+//         if (arr[i]===reversedArray[j]) {
+//             console.log("The given string is in palindrome.")
+            
+//         }
+//         else{
+//             console.log("The given string is not in palindrome.")
+//         }
+        
+//     }
+    
+// }
+
+// let palindrome = "MADAM";
+
+
+// let word = Array.from(palindrome);
+// console.log(word)
+// reverseWord = word.slice();
+// reversedArray = reverseWord.reverse();
+// console.log(reversedArray);
+
+// if (palindrome===reversedArray.join("")){
+//     console.log("Yes this is a palindrome.")
+// }
+// else{
+//     console.log("No this is not a palindrome.");
+    
+// }
+
+
+
+
+
+// let arr = [1,2,3,4];
+// let j = 0
+// for (i = 0;i<arr.length;i++){
+//     j = j+arr[i];
+
+// }
+
+
+// console.log(j);
